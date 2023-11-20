62d73a9a8c8c1c5f3a9cf72a289288898dfdf736
youchiii <mochizuki.yousei.infratop@gmail.com>
2023-11-20 07:36:50 +0000

    スレッド一覧、投稿ごとのページ　完成

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -5,16 +5,18 @@ class SubjectsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    #@list.user_id = current_user.id
-    @list.save
-    redirect_to '/subjects'
+    @list.user_id = current_user.id
+    @list.save!　#びっくりマーク付けると保存できない原因がエラー文でわかる
+    @lists = List.all
+    render :index
   end
 
   def index
-    @lists = List.all
+    @lists = List.all #lists複数形は配列的な感じ
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
