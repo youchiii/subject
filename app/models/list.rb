19dadf56144a96a5e3d4a329413dcd70f053271e
youchiii <mochizuki.yousei.infratop@gmail.com>
2023-11-27 05:43:48 +0000

    投稿のバリデーションを実装しました

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,8 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  validates :title, presence: true #エラーメッセージを表示するためのバリデーション
+  validates :body, presence: true #エラーメッセージを表示するためのバリデーション
   def change
     create_table :lists do |t|
       t.string :title
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   #get 'homes/top'
   devise_for :users
   get "subjects/top" => "subjects#top"
+  get "homes/about" => "homes#about"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   resources :users, only: [:show, :edit,:update]
+  #resources :homes, only: [:top, :about]
   resources :subjects, only: [:new, :create, :index, :show, :destroy, :top] do
     resource :favorite, only: [:create, :destroy]#resourceにすると/idがURLに含まれなくなる。
     resources :comments, only: [:create, :show, :destroy]
