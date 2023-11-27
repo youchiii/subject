548b93799d75bded26b47493cfbfbd53450c3038
youchiii <mochizuki.yousei.infratop@gmail.com>
2023-11-27 05:49:41 +0000

    ユーザ名のバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :lists, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  validates :name, presence: true
 end
